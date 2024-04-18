8cea509280a31f473aa42cc9e271b80bcb7700d4
mingssii <6633267521@student.chula.ac.th>
2024-04-18 16:22:14 +0700

    getmenus

--- backend/controllers/menus.js
+++ backend/controllers/menus.js
@@ -19,80 +19,105 @@ exports.getMenus = async (req, res, next) => {
     }
 }
 
-// exports.addMenu = async (req, res, next) => {
+exports.getMenu = async (req, res, next) => {
+    try {
+        const menu = await Menu.findById(req.params.id).populate({
+            path: 'restaurant',
+            select: 'name description tel'
+        })
 
-//     try {
-//         req.body.restaurant = req.params.restaurantId;
+        if (!menu) {
+            return res.status(404).json({ success: false, message: `No reservation with the id of ${req.params.id}` })
+        }
 
-//         const restaurant = await Restaurant.findById(req.params.restaurantId);
+        res.status(200).json({
+            success: true,
+            data: menu
+        })
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: 'Cannot find Menu' });
+    }
+}
 
-//         if (!restaurant) {
-//             return res.status(404).json({ success: false, message: `No restaurant with the id of ${req.params.restaurantId}` });
-//         }
-//         console.log(req.body)
-
-//         req.body.user = req.user.id;
-
-//         const menu = await Menu.create(req.body);
-//         res.status(201).json({
-//             success: true,
-//             data: menu
-//         })
-//     }
-//     catch (error) {
-//         console.log(error);
-//         return res.status(500).json({ success: false, message: 'Cannot create Menu' });
-//     }
-
-// };
-
-// exports.updateMenu = async (req, res, next) => {
-
-//     try {
-
-//         let menu = await Menu.findById(req.params.id)
-
-//         if (!menu) {
-//             res.status(404).json({ success: false, message: `No menu with the id of ${req.params.id}` });
-//         }
-
-//         if (req.user.role !== 'admin') {
-//             return res.status(401).json({ success: false, message: `User ${req.user.id} is not authorized to update this menu` });
-//         }
-
-//         menu = await Menu.findByIdAndUpdate(req.params.id, req.body, {
-//             new: true,
-//             runValidators: true
-//         });
+exports.addMenu = async (req, res, next) => {
+    //console.log(req.body);
 
 
-//         res.status(200).json({ success: true, data: menu });
-//     }
-//     catch (error) {
-//         console.log(error)
-//         res.status(500).json({ success: false, message: 'Cannot update menu' });
-//     }
+    try {
+        //console.log( req.body.restaurant);
+        req.body.restaurant = req.params.restaurantId;
 
-// };
+        const restaurant = await Restaurant.findById(req.params.restaurantId);
 
-// exports.deleteMenu = async (req, res, next) => {
+        if (!restaurant) {
+            return res.status(404).json({ success: false, message: `No restaurant with the id of ${req.params.restaurantId}` });
+        }
+        console.log(req.body)
+
+        req.body.user = req.user.id;
+
+        const menu = await Menu.create(req.body);
+        res.status(201).json({
+            success: true,
+            data: menu
+        })
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: 'Cannot create Menu' });
+    }
+
+};
+
+exports.updateMenu = async (req, res, next) => {
+
+    try {
+
+        let menu = await Menu.findById(req.params.id)
+
+        if (!menu) {
+            res.status(404).json({ success: false, message: `No menu with the id of ${req.params.id}` });
+        }
+
+        if (req.user.role !== 'admin') {
+            return res.status(401).json({ success: false, message: `User ${req.user.id} is not authorized to update this menu` });
+        }
+
+        menu = await Menu.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
 
 
-//     try {
-//         const menu = await Menu.findById(req.params.id);
+        res.status(200).json({ success: true, data: menu });
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).json({ success: false, message: 'Cannot update menu' });
+    }
 
-//         if (!menu) {
-//             return res.status(404).json({ success: false, message: `No menu with the id of ${req.params.id}` });
-//         }
+};
 
-//         await menu.deleteOne();
-//         res.status(200).json({ success: true, data: {} });
-//     }
-//     catch (error) {
-//         console.log(error)
-//         res.status(500).json({ success: false, message: 'Cannot delete Menu' });
-//     }
-// };
+exports.deleteMenu = async (req, res, next) => {
+
+
+    try {
+        const menu = await Menu.findById(req.params.id);
+
+        if (!menu) {
+            return res.status(404).json({ success: false, message: `No menu with the id of ${req.params.id}` });
+        }
+
+        await menu.deleteOne();
+        res.status(200).json({ success: true, data: {} });
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).json({ success: false, message: 'Cannot delete Menu' });
+    }
+};
 
 
 
