1486d629ae987bff5d212bded16db7f77133f19d
KanitSub <156632864+KanitSub@users.noreply.github.com>
2024-04-19 01:19:06 +0700

    Pay many products

--- backend/controllers/transactions.js
+++ backend/controllers/transactions.js
@@ -2,29 +2,30 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Transaction = require('../models/Transaction');
 const {v4:uuidv4} = require('uuid')
 exports.checkout = async (req, res, next) => {
-    const {user, product} = req.body
+    const {user, products} = req.body
     const orderId = uuidv4()
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types:['card'],
-        line_items: [
-          {
-            price_data:{
-                currency:'thb',
-                product_data:{
-                    name: product.name,
-                },
-                unit_amount: product.price * 100,
-            },
-            quantity: product.quantity,
+
+    const lineItems = products.map(product => ({
+      price_data: {
+          currency: 'thb',
+          product_data: {
+              name: product.name,
           },
-        ],
+          unit_amount: product.price * 100,
+      },
+      quantity: product.quantity,
+  }));
+
+   const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: lineItems,
         mode: 'payment',
-        //url to success page
+        // URL to success page
         success_url: `${process.env.FRONTEND_URL}/payment/successful`,
-        //url to fail page
+        // URL to fail page
         cancel_url: `${process.env.FRONTEND_URL}/payment/fail`,
-      });
-    
+    });
+
       const orderData = {
         fullname: user.name,
         address: user.address,
@@ -36,7 +37,7 @@ exports.checkout = async (req, res, next) => {
       const transaction = await Transaction.create(orderData)
     
       res.json({
-        product,
+        products,
         orderId,
         transaction
       })
