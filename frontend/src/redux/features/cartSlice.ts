22981ab643a57754ffccd20d8911ad98f295224f
Poomrapee-chsk <72385271+Poomrapee-chsk@users.noreply.github.com>
2024-04-21 22:45:15 +0700

    edited cartslice and added functionalities to add food to the redux store

--- frontend/src/redux/features/cartSlice.ts
+++ frontend/src/redux/features/cartSlice.ts
@@ -1,69 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { ReservationItem } from "../../../interfaces";
+import { FoodItem } from "../../../interfaces";
 import { PayloadAction } from "@reduxjs/toolkit";
-import newReservation from "@/libs/newReservation";
 import deleteReservation from "@/libs/deleteReservation";
 import updateReservation from "@/libs/updateReservation";
 
-
-
 type CartState = {
-  restaurantItems: ReservationItem[];
-};
+  foodItems: FoodItem[];
+}
 
-
-const initialState: CartState = { restaurantItems: [] };
-
+const initialState:CartState = {
+  foodItems: [],
+}
 
 export const cartSlice = createSlice({
   name: "Cart",
-  initialState,
+  initialState: initialState,
   reducers: {
-    addReservation:  (state, action: PayloadAction<ReservationItem>) => {
-        const newReservationItem = action.payload;
-        const token = newReservationItem.token;
-        const userid = newReservationItem.user
-
-        const create = async()=>{
-            console.log(token)
-            if(newReservationItem.restaurantId && token){
-                console.log('pong')
-                await newReservation(newReservationItem.restaurantId,token,newReservationItem)
-                console.log(',,,')
-            }
-        }
-
-        console.log(userid);
-        create();
-        
+    addReservation:  (state = initialState, action: PayloadAction<FoodItem>) => {
+      if (!state.foodItems) {
+        state.foodItems = []
+      }
+      state.foodItems.push(action.payload)
     },
-    removeReservation:  (
-      state,
-      action: PayloadAction<{ id: string; token: string }>
-    ) => {
-        console.log(action.payload.token)
-        console.log(action.payload.id)
-        const deleteRes = async()=>{
-            const res = await deleteReservation(action.payload.token,action.payload.id);
-            window.location.reload()
-        }
-        deleteRes();
+    removeReservation:  (state, action: PayloadAction<{ fname: string }>) => { //<-- dispatch(removeReservation({fname: ชื่ออาหาร}))
+      const remainItems = state.foodItems.filter(obj =>{
+        return (
+          obj.name !== action.payload.fname
+        )
+      })
+      state.foodItems = remainItems
     },
-    editReservation: (state,action : PayloadAction<{id:string; token : string; item:object}>) =>{
-        console.log('Update')
-        console.log(action.payload.token)
-        console.log(action.payload.id)
-        const update = async()=>{
-            const res = await updateReservation(action.payload.id,action.payload.token,action.payload.item)
-            window.location.reload()
-        }
-        update()
-    }
+    // editReservation: (state,action : PayloadAction<{id:string; token : string; item:object}>) =>{
+    //     console.log('Update')
+    //     console.log(action.payload.token)
+    //     console.log(action.payload.id)
+    //     const update = async()=>{
+    //         const res = await updateReservation(action.payload.id,action.payload.token,action.payload.item)
+    //         window.location.reload()
+    //     }
+    //     update()
+    // }
   },
 });
 
 
-export const { addReservation, removeReservation ,editReservation} = cartSlice.actions;
+export const { addReservation, removeReservation } = cartSlice.actions;
 export default cartSlice.reducer;
 
 
