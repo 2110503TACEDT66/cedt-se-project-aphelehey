25c60ecf3f5319ce2c0bedb78e563e6ab658d27d
Poomrapee-chsk <72385271+Poomrapee-chsk@users.noreply.github.com>
2024-04-21 22:56:02 +0700

    food cart page added

--- frontend/src/components/ReservationCart.tsx
+++ frontend/src/components/ReservationCart.tsx
@@ -1,73 +1,63 @@
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { removeReservation } from "@/redux/features/cartSlice";
-import { ReservationItem ,ReservationJson} from "interfaces";
-import getReservations from "@/libs/getReservations";
-import Link from "next/link";
-import dayjs from "dayjs";
+import { FoodItem, OrderItem} from "interfaces";
+import styles from '@/components/reservationcart.module.css'
+import Image from "next/image";
+import { addReservation, removeReservation } from "@/redux/features/cartSlice";
+import getMenu from "@/libs/getMenu";
+import getMenus from "@/libs/getMenus";
+import { useAppSelector } from "@/redux/store";
 
 export default function ReservationCart() {
-  const [reservationItems, setReservationItems] = useState<ReservationItem[]>([]); // Initialize as empty array
+  // const [reservationItems, setReservationItems] = useState<OrderItem[]>([]); // Initialize as empty array
+  const [menuItems, setMenuItems] = useState<FoodItem[]>([])
   const { data: session } = useSession();
   const token = session?.user.token;
+  const user = session?.user._id
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (token) {
-        const fetchedData : ReservationJson = await getReservations(token);
-        console.log(fetchedData.data);
-        if (fetchedData) {
-          setReservationItems(fetchedData.data);
-        }
-      }
-    };
-    fetchData();
-  }, [token]);
+  const reservationItems = useAppSelector((state)=>
+    state.cartSlice.foodItems
+  )
 
-  const handleRemove=(reservationItem:ReservationItem)=>{
-    if(reservationItem._id && token){
-        const item={
-            id: reservationItem._id,
-            token:session?.user.token
-          }
-          dispatch(removeReservation(item));
+  const handleRemove=(reservationItem:FoodItem)=>{
+    if(reservationItem.name && token && user){
+        const name = reservationItem.name
+          dispatch(removeReservation({fname: name}));
     }
   }
 
-  const handleUpdate = () =>{
-
-  }
-  
-
   return (
     <>
-      {reservationItems.length === 0 ? (
-        <p>No Reservation</p>
+      {reservationItems?.length === 0 || !reservationItems ? (
+        <><p>No Reservation</p>
+        </>
       ) : (
-        reservationItems.map((reservationItem:ReservationItem) => (
-          <div
-            className="bg-slate-200 rounded px-5 mx-5 py-2 my-2"
-            key={reservationItem.user}>
-            <div className="text-xl">{reservationItem.restaurant?.name}</div>
-            <div className="text-sm">Reserve at {dayjs(reservationItem.revDate).format('dddd, MMMM D, YYYY h:mm A')} </div>
-            <div className="flex flex-row">
-                <button
-                className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2
-                text-white shadow-sm w-50"
-                onClick={() =>handleRemove(reservationItem)}
-                >
-                Remove
-                </button>
-                <Link href={`/cart/${reservationItem._id}`}>
-                <button
-                className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2
-                text-white shadow-sm ml-2 w-50"
-                >
-                Edit
-                </button>
-                </Link>
+        reservationItems?.map((reservationItem:FoodItem) => (
+          <div key={reservationItem.name} className="flex my-10 pl-5 mr-5">
+            <Image src={`/img/${reservationItem.picture}.jpg`} alt="Image Placement Here" width={200} height={200} className={styles.image}></Image>
+            <div
+            className="border-solid border-2 border-slate-300 w-[100%] pr-5">
+              <div className="flex h-full">
+                <div className="flex w-4/5">
+                    <div className="flex flex-col">
+                      <div className="text-4xl p-5">
+                          {reservationItem.name}
+                      </div>
+                      <div className="text-2xl px-5">
+                          {`${reservationItem.price} ฿`}
+                      </div>
+                    </div>
+                </div>
+                <div className="grid place-items-end w-1/5">
+                    <button className="block rounded-md bg-red-700 hover:bg-red-500 px-5 py-3 my-5 mx-5 text-white shadow-sm ml-2 w-50"
+                    onClick={()=>{handleRemove(reservationItem)}}
+                    >
+                      Remove
+                    </button>
+                </div>
+              </div>
             </div>
           </div>
         ))
@@ -75,3 +65,84 @@ export default function ReservationCart() {
     </>
   );
 }
+
+  // REMOVE BEFORE MERGING TO MAIN
+
+        /* ADD FOOD TO CART TEST 
+        
+        // const handleAdd = () => {
+          //   dispatch(addReservation({
+          //     name: "pizza",
+        //     price: 200,
+        //     picture: "pizza"
+        //   }))
+        // }
+
+        //=====================================================================
+        <button className="block rounded-md bg-red-700 hover:bg-red-500 px-5 py-3 my-5 mx-5 text-white shadow-sm ml-2 w-50"
+          onClick={() => {handleAdd()}}>
+          Test Add
+        </button> */
+
+        //=====================================================================
+         //Function to look up a price (not needed anymore)
+        // const priceAndPicLook = (fname: string) => {
+        //   const match = menuItems.find((menu) => menu.name === fname) 
+        //   return {price: match?.price, picture: match?.picture}
+        // }
+
+    //===========================================================================
+    // UseEffect is not needed because we store data in the local storage.
+    // useEffect(() => {
+    // const fetchData = async () => {
+    //   if (token) {
+    //     // const fetchDataJson = await getAllOrders(token);
+    //     // const fetchedData:OrderItems[] = fetchDataJson.data
+    //     const fetchedData:OrderItem[] = [ //<-- Just a mock data to test. There shouldn't be duplicate foods.
+    //     {
+    //       _id: "2411244113214",
+    //       food: ["Pizza","Pizzer","Pizzest"],
+    //       price: 200,
+    //       payment: true,
+    //       restaurant: "65e44c37cb8aa54383faa2d2",
+    //     },
+    //   ]
+    //     console.log(fetchedData);
+    //     if (fetchedData) {
+    //       setReservationItems(fetchedData);
+    //     }
+    //   }
+
+      // const fetchMenu = async () => {
+      // //const fetchedMenu = await getMenus()
+      // Mock Data again
+      // const fetchedMenu:FoodItem[] = [
+      //   {
+      //    name: "Pizza",
+      //    price: 100,
+      //    picture: "pizza"
+      //   },
+      //   {
+      //     name: "Pizzer",
+      //     price: 200,
+      //     picture: "pizza"
+      //   },
+      //   {
+      //     name: "Pizzest",
+      //     price: 300,
+      //     picture: "pizza"
+      //   }
+      // ]
+
+      // if (fetchedMenu) {
+      //   setMenuItems(fetchedMenu);
+      // }
+    // }
+    // fetchData();
+    // fetchMenu();
+
+    // };
+
+    
+
+    
