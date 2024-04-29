fc1f2fb0d2abc5341b6a9c35ec52aa5c5400ccc3
mingssii <6633267521@student.chula.ac.th>
2024-04-29 15:32:50 +0700

    Add delivery bar to select restaurant page

--- frontend/src/app/(carinfo)/car/page.tsx
+++ frontend/src/app/(carinfo)/car/page.tsx
@@ -3,20 +3,20 @@ import CarCatalog from "@/components/CarCatalog"
 import { Suspense } from "react"
 import { LinearProgress } from "@mui/material"
 import { RestaurantItem, RestaurantJson } from "interfaces"
+import DeliveryBar from "@/components/DeliveryBar"
 
 
 export default function Car() {
     const restaurants = getRestaurants()
     return (
         <main className="text-center p-5 bg-slate-100">
-            <h1 className="text-2xl font-medium text-black bold">Select Restaurant</h1>
+            <h1 className="text-2xl font-medium text-black bold">Order with us</h1>
+            <h1 className="text-xl font-medium text-black bold">Select Restaurant</h1>
             <Suspense fallback={<p>Loading ... <LinearProgress /></p>}>
                 <CarCatalog restaurantJson={restaurants} />
             </Suspense>
-
-            {/* <hr className="my-10"></hr>
-            <h1 className="text-xl font-medium">Try Client-side Car Panel</h1>
-            <CarPanel /> */}
+            <h1 className="text-xl font-medium text-black bold">Order with delivery Service</h1>
+            <DeliveryBar/>
         </main>
     )
 }
