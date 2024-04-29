07dbe5d44daead2d1bbfff5551176a736523eb72
MyChadum07 <156446728+MyChadum07@users.noreply.github.com>
2024-04-29 22:10:24 +0700

    delete กลับมาแล้ว :0

---
+++ frontend/src/libs/deleteAddress.tsx
@@ -0,0 +1,22 @@
+export default async function deleteAddress(token: string, userId: string,index:number) {
+    const body = JSON.stringify({
+        user:userId,
+        index:index
+    })
+    const response = await fetch(`http://localhost:5000/api/v1/userAddresses`, {
+       
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+        body: body
+    });
+
+    if (!response.ok) {
+        const errorMessage = await response.text(); // Capture detailed error message
+        throw new Error(`Failed to delete address: ${errorMessage}`);
+    }
+
+    console.log(`Address with ID: ${userId} deleted successfully.`); // Log success message
+}
