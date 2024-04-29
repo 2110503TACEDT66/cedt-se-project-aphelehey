f703f8a5396906f68f125c1715d8deae7f50635e
Imnotmetch <main.thanametch.klao@gmail.com>
2024-04-29 23:31:51 +0700

    US2-3 maybe add sth later na

---
+++ frontend/cypress/e2e/US2-3.cy.ts
@@ -0,0 +1,27 @@
+before(()=>{
+    cy.visit('http://localhost:3000/')
+  })
+
+describe('US2-2', () => {
+
+    const waitTime = 2000
+
+    it('Menus', () => {
+        cy.visit('http://localhost:3000/api/auth/signin')
+        cy.get('#input-email-for-credentials-provider').type('Metch@gmail.com')
+        cy.get('#input-password-for-credentials-provider').type('123456')
+        cy.get('button').click()
+
+        cy.wait(waitTime)
+        cy.get('.bg-black').click()
+        cy.get('[href="/car/65e44c37cb8aa54383faa2d2"]').click()
+    
+        cy.wait(waitTime/2)
+        cy.get('.object-cover').first().click()
+        cy.get('.MuiButtonBase-root').click()
+
+        cy.wait(waitTime)
+        cy.contains('body','king').should('be.visible');
+
+    })
+})
